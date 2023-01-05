4c863f141c285b6b76b3f7fa8a526937d07ed095
unknown <samdshakilofficial@gmail.com>
2023-01-06 00:14:29 +0500

    add fext name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import router from "./Routes/Routes/Routes";
 function App() {
   return (
     <div className="max-w-[1440px] mx-auto">
-      <h5>shakil monsi</h5>
+      {/* <h5>shakil monsi</h5> */}
       <RouterProvider router={router}></RouterProvider>
       <Toaster></Toaster>
     </div>
